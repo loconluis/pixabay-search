83d7899da63894f188f68349c7027592176aaa2b
loconluis <luis.locon@gmail.com>
2017-10-09 23:06:08 -0600

    Agregando estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { MasonryModule } from 'angular2-masonry';
+import { MatCardModule, MatInputModule, MatButtonModule, MatProgressBarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { ImageListComponent } from './image-list/image-list.component';
@@ -15,7 +18,13 @@ import { ImageService } from './services/image.service';
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MasonryModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressBarModule
   ],
   providers: [ImageService],
   bootstrap: [AppComponent]
